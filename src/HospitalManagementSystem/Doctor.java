d1de87d59493f2194a73b0565b4281bcc6f67157
Poonam Mashalakar <121446632+pmashalakar@users.noreply.github.com>
2024-02-15 23:20:06 +0530

    Add files via upload

---
+++ src/HospitalManagementSystem/Doctor.java
@@ -0,0 +1,56 @@
+package HospitalManagementSystem;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Doctor {
+    private Connection connection;
+
+    public Doctor(Connection connection){
+        this.connection = connection;
+    }
+
+    public void viewDoctors(){
+        String query = "select * from doctors";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            System.out.println("Doctors: ");
+            System.out.println("+------------+--------------------+------------------+-------------------+");
+            System.out.println("| Doctor Id  | Name               | Specialization   | phone_no          |");
+            System.out.println("+------------+--------------------+------------------+-------------------+");
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String specialization = resultSet.getString("specialization");
+                String phone_no = resultSet.getString("phone_no");
+                System.out.printf("| %-10s | %-18s | %-16s | %-17s |\n", id, name, specialization,phone_no);
+                System.out.println("+------------+--------------------+------------------+-------------------+");
+            }
+
+        }catch (SQLException e){
+        	
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getDoctorById(int id){
+        String query = "SELECT * FROM doctors WHERE id = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
